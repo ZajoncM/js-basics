e69dd2af60ab71eb857eeab6e736ea30073297cd
ZajoncM <milosz.zajonc@gmail.com>
2021-12-06 14:26:33 +0100

    nit: add alternative solution for flatten function

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -1,3 +1,9 @@
-const flatten = (array) => array.flat(Infinity);
+// const flatten = (array) => array.flat(Infinity);
+
+const flatten = (array) => {
+  return array.reduce((flat, value) => {
+    return flat.concat(Array.isArray(value) ? flatten(value) : value);
+  }, []);
+};
 
 module.exports = flatten;
